b9f257fb2a10ddbc0eaec561e70368260036acbb
johncabang <johncabz@gmail.com>
2020-10-06 21:23:22 +0000

    Updated tweet container, aligning username & user handle

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-  $("#tweet-text").keyup(function() {
+  $('#tweet-text').keyup(function() {
     let counter = 140;
     let length = $(this).val().length;
-    $(".counter").text(counter - length);
+    
+    $('.counter').text(counter - length);
     if (counter < length) {
-      $(".counter").css("color", "#FF0000") 
+      $('.counter').css("color", "#FF0000") 
     } else {
-      $(".counter").css("color", "#545149");
+      $('.counter').css("color", "#545149");
     }
   })
+      // $(this).nextAll('span').text(counter - length);
+      // if (counter < length) {
+      //   $(this).nextAll('span').css("color", "#FF0000") 
+      // } else {
+      //   $(this).nextAll('span').css("color", "#545149");
+      // }
 }); 
+
+
