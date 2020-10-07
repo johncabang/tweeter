dfec9adb71e6bf35c1386b88459e85eb9e97c75c
johncabang <johncabz@gmail.com>
2020-10-07 00:48:14 +0000

    Updated tweeter icons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,47 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+
+
+
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+};
+
+const createTweetElement = function(tweet) {
+  // let $tweet = /* Your code for creating the tweet element */
+  // ...
+  // return $tweet;
+};
+
+renderTweets(data);
+
+
+
+
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK,png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$tweet('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
